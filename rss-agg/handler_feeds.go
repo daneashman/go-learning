777451f112f8a3d1f581e77d2782b959e4ced2d0
Dane Ashman <daneashman@outlook.com>
2026-01-17 23:45:41 -0800

    cleaned up create feed handler

--- rss-agg/handler_feeds.go
+++ rss-agg/handler_feeds.go
@@ -10,36 +10,20 @@ import (
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
-// type Res struct {
-// 	message string
-// }
-
-// func buildRes(message string) (string, error) {
-// 	// Create response struct
-// 	res := Res{
-// 		message: "message"
-// 	}
-//
-// 	// Marshal to JSON
-// 	resJson, err := json.Marshal(res)
-// 	if err != nil {
-// 		return "", err
-// 	}
-//
-// 	return resJson, nil
-// }
-
 func httpRes(w http.ResponseWriter, code int, res []byte) {
-	w.WriteHeader(500)
-	w.Write([]byte("Error connecting to database."))
+	w.WriteHeader(code)
+	_, err := w.Write(res)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func handleCreateFeed(w http.ResponseWriter, _ *http.Request) {
 	// Connect to db
 	db, err := sql.Open("pgx", os.Getenv("DB_URI"))
 	if err != nil {
-		w.WriteHeader(500)
-		w.Write([]byte("Error connecting to database."))
+		httpRes(w, 500, []byte(fmt.Sprintf("%v", err)))
+		return
 	}
 	defer db.Close()
 
@@ -51,23 +35,7 @@ func handleCreateFeed(w http.ResponseWriter, _ *http.Request) {
 	q := fmt.Sprintf(sqlInsertFeed, "one", "one.com")
 	_, err = db.Query(q)
 	if err != nil {
-		w.WriteHeader(400)
-		w.Write([]byte(fmt.Sprintf("%v", err)))
-	}
-
-	val, err := json.Marshal(struct{
-		Name string
-		Url string
-	}{
-		"feed",
-		"example.com",
-	})
-	if err != nil {
-		log.Printf("Error marshaling json: %s\n", err)
-	}
-
-	_, err = w.Write(val)
-	if err != nil {
-		log.Printf("Error writing response: %s\n", err)
+		httpRes(w, 400, []byte(fmt.Sprintf("%v", err)))
+		return
 	}
 }
