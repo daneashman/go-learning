7ddc8b6ab7b74c3e033dd3d726baa8a4a1964c69
Dane Ashman <daneashman@outlook.com>
2026-01-18 16:26:11 -0800

    polished the create feed handler

--- rss-agg/handler_feeds.go
+++ rss-agg/handler_feeds.go
@@ -6,9 +6,15 @@ import (
 	"log"
 	"os"
 	"fmt"
+	"io"
 	"database/sql"
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
+
+type ReqBody struct {
+	FeedName	string	`json:"feed_name"`
+	FeedUrl		string	`json:"feed_url"`
+}
 
 func httpRes(w http.ResponseWriter, code int, res []byte) {
 	w.WriteHeader(code)
@@ -18,7 +24,7 @@ func httpRes(w http.ResponseWriter, code int, res []byte) {
 	}
 }
 
-func handleCreateFeed(w http.ResponseWriter, _ *http.Request) {
+func handleCreateFeed(w http.ResponseWriter, req *http.Request) {
 	// Connect to db
 	db, err := sql.Open("pgx", os.Getenv("DB_URI"))
 	if err != nil {
@@ -27,12 +33,27 @@ func handleCreateFeed(w http.ResponseWriter, _ *http.Request) {
 	}
 	defer db.Close()
 
+	// Pull data from req body
+	bodyRaw, err := io.ReadAll(req.Body)
+	if err != nil {
+		httpRes(w, 500, []byte(fmt.Sprintf("%v", err)))
+		return
+	}
+
+	// Unmarshal req body into ReqBody struct
+	var body ReqBody
+	err = json.Unmarshal(bodyRaw, &body)
+	if err != nil {
+		httpRes(w, 500, []byte(fmt.Sprintf("%v", err)))
+		return
+	}
+
 	// Insert data into db
 	sqlInsertFeed := `
 	INSERT INTO feeds
 	VALUES (DEFAULT, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '%s', '%s');
 	`
-	q := fmt.Sprintf(sqlInsertFeed, "one", "one.com")
+	q := fmt.Sprintf(sqlInsertFeed, body.FeedName, body.FeedUrl)
 	_, err = db.Query(q)
 	if err != nil {
 		httpRes(w, 400, []byte(fmt.Sprintf("%v", err)))
